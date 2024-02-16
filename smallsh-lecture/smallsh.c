457202658c016a1fdd838504018fe1b1b0744563
joshuajk7 <35845463+joshuajk7@users.noreply.github.com>
2024-02-15 21:46:42 -0800

    Add files via upload

--- smallsh-lecture/smallsh.c
+++ smallsh-lecture/smallsh.c
@@ -7,6 +7,11 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <inttypes.h>
+#include <fcntl.h>
 
 #ifndef MAX_WORDS
 #define MAX_WORDS 512
@@ -30,16 +35,79 @@ int main(int argc, char *argv[])
 
   char *line = NULL;
   size_t n = 0;
+  
+  /* Save original sigactions to restore for child process */
+  struct sigaction SIGINT_oldact;
+  struct sigaction SIGTSTP_oldact;
+
+  /* Creating sigaction structs for SIGINT and SIGTSTP handling */
+  struct sigaction SIGINT_action = {0}, ignore_action = {0};
+  // ignore SIGSTP in interactive mode
+  ignore_action.sa_handler = SIG_IGN;
+  sigfillset(&ignore_action.sa_mask);
+  // No flags set
+  ignore_action.sa_flags = 0;
+  // SIGINT uses sigint_handler
+  SIGINT_action.sa_handler = sigint_handler;
+  sigfillset(&SIGINT_action.sa_mask);
+  // No flags set
+  SIGINT_action.sa_flags = 0;
+  
   for (;;) {
-//prompt:;
+prompt:;
     /* TODO: Manage background processes */
-
-    /* TODO: prompt */
+    int bgFlag = 0;
+    pid_t bgChildPid;
+    int bgChildStatus;
+    int corrBgStatus;
+    while ((bgChild = waitpid(0, &bgChildStatus, WNOHANG | WUNTRACED)) > 0) {
+      if (WIFEXITED(bgChildStatus)) {
+        corrBgStatus = WEXITSTATUS(bgChildStatus);
+        fprintf(stderr, "Child process % done. Exity status %d. \n",
+            (intmax_t)bgChildPid, corrBgStatus);
+      }
+      else if (WIFSIGNALED(bgChildStatus)) {
+        corrBgStatus = WTERMSIG(bgChildStatus);
+        fprintf(stderr, "Child process %jd done. Signaled %d./n",
+            (intmax_t)bgChildPid, corrBgStatus);
+      }
+      else if (WIFSTOPPED(bgChildStatus)) {
+        fprintf(stderr, "Child process %jd stopped. Continuing.\n", 
+            (intmax_t)bgChildPid);
+            kill(bgChildPid, 18);
+      }
+    }
+  
     if (input == stdin) {
-
+    
+      /* Print prompt for interactive mode */
+      char *prompt = getenv("PS1");
+      if (prompt == NULL) {
+        prompt = "";
+      }
+      fprintf(stderr, "%s", prompt);
+    
+      /* Handle Signals */
+      sigaction(SIGTSTP, &ignore_action, &SIGTSTP_oldact);
+      sig(SIGINT, &SIGINT_action, &SIGINT_oldact);
     }
     ssize_t line_len = getline(&line, &n, input);
-    if (line_len < 0) err(1, "%s", input_fn);
+    if (feof(input) != 0) {
+      exit(0);
+    }
+    
+    if (line_len < 0) {
+      /* Handle ^C interrupt*/
+      if (errno == EINTR) {
+        clearerr(input);
+        fprintf(stderr, "/n");
+        goto prompt;
+      }
+      err(1, "%s", input_fn);
+    }
+    if (line_len == 1 && strcmp(line, "\n") == 0) {
+      continue;
+    }
     
     size_t nwords = wordsplit(line);
     for (size_t i = 0; i < nwords; ++i) {
@@ -47,13 +115,181 @@ int main(int argc, char *argv[])
       char *exp_word = expand(words[i]);
       free(words[i]);
       words[i] = exp_word;
-      fprintf(stderr, "Expanded Word %zu: %s\n", i, words[i]);
     }
-  }
-}
+    
+    // set SIGINT to ignore in interactive mode
+    if (input == stdin) {
+      sigaction(SIGINT, &ignore_action, NULL);
+    }
+    
+    if (strcmp(words[nwords - 1], "&") == 0) {
+      nwords--;
+      bgFlag = 1;
+    }
+    
+    /*CD function */
+    if (strcmp(words[0], "cd") == 0) {
+      if (nwords > 1) {
+        if (chdir(words[1]) == -1 {
+          fprintf(stderr, "directory not found\n");
+        }
+      }
+      else if (chdir(getenv("HOME")) == -1) {
+        fprintf(stderr, "directory not found\n");
+      }
+      continue;
+    }
+   
+    /* Exit function */
+    if (strcmp(words[0], "exit") == 0) {
+      if (words > 2) {
+        fprintf(stderr, "too many arguments\n");
+        goto prompt;
+      }
+      else if (nwords == 2) {
+        char *next;
+        long value;
+        value = strtol(words[1], &next, 10);
+        if ((next == words[1]) || (*next != '\0')) {
+          fprintf(stderr, "that is not a number\n");
+          goto prompt;
+        }
+        else {
+          exit(value);
+        }  
+      }
+      else {
+        char *exp_word = expand("$?");
+        for (;;) exit(atoi(exp_word))
+      }
+    }
+    
+    int childStatus;
+    pid_t childPid = fork();
+
+    switch(childPid) {
+      case -1:
+        perror("fork() failed!");
+        continue;
+      case 0: {
+        // child process
+        /* change the sigactions */
+        if (input == stdin) {
+          sigaction(SIGINT, &SIGINT_oldact, NULL);
+          sigaction(SIGTSTP, &SIGTSTP_oldact, NULL);
+        }
+        
+        /* Parsing the words list */
+        char* args[nwords + 1];
+        size_t args_len = 0;
+        int childInput;
+        int childOutput;
+        for (size_t i = 0; i < nwords; ++i) {
+          /* Redirected input file */
+          if (strcmp(words[i], "<") == 0) {
+            if (strcmp(words[i + 1], "")) {
+              childInput = open(words[i + 1], O_RDONLY | O_CLOEXEC);
+              if (childInput == -1) {
+                perror("failed to open file");
+                exit(1);
+              }
+              int result = dup2(childInput, 0);
+              if (result == -1) {
+                perror("failed to assign new input file");
+                exit(2);
+              }
+              // skip both the < and the input file
+              i++;
+              continue;
+            }
+          }
+          /* Redirected output file truncated */ 
+          else if (strcmp(words[i], ">") == 0) {
+            if (strcmp(words[i + 1], "")) {
+              childOutput = open(words[i + 1], 
+                  O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0777);
+              if (childOutput == -1) {
+                perror("failed to create new or open old output file");
+                exit(1);
+              }
+              int result = dup2(childOutput, 1);
+              if (result == -1) {
+                perror("failed to assign new output file");
+                exit(2);
+              }
+              // skip both the > and the output file
+              i++;
+              continue;
+            }
+          }
+          
+          /* Redirected output file appended */
+          else if (strcmp(words[i], ">>") == 0) {
+            if (strcmp(words[i + 1], "")) {
+              childOutput = open(words[i + 1], 
+                  O_WRONLY | O_CREAT | O_APPEND | O_CLOEXEC, 0777);
+              if (childOutput == -1) {
+                perror("failed to create new or open old output file");
+                exit(1);
+              }
+              int result = dup2(childOutput, 1);
+              if (result == -1) {
+                perror("failed to assign new output file");
+                exit(2);
+              }
+              // skip both the >> and the output file
+              i++;
+              continue;
+            }
+          }
+          
+          else {
+            args_len++;
+            args[i] = words[i];
+          }
+        }
+        args[args_len] = NULL;
+        
+        execvp(args[0], args);
+        perror("smallsh");
+        _exit(EXIT_FAILURE);
+        break;
+      }
+      default: {
+        if (!bgFlag) {
+          waitpid(childPid, &childStatus, WUNTRACED);
+          int corrStatus;
+          if (WIFSIGNALED(childStatus)) {
+            corrStatus = WTERMSIG(childStatus) + 128;
+          }
+          else if (WIFSTOPPED(childStatus)) {
+            // Stop signal was given
+            char* bgPid;
+            asprintf(&bgPid, "%jd", (intmax_t) childPid);
+            setenv("BG_PID", bgPid, 1);
+            fprintf(stderr, "Child process %jd stopped. Continuing.\n", (intmax_t)childPid); 
+            kill(childPid, 18);  // Signal 18 = SIGCONT
+            goto prompt;
+          }
+          else if (WIFEXITED(childStatus)) corrStatus = WEXITSTATUS(childStatus);
+          else corrStatus = WTERMSIG(childStatus);
+          char* childStatusStr;
+          asprintf(&childStatusStr, "%d", corrStatus);
+          setenv("LATEST_FG", childStatusStr, 1);
+
+        }
+        else {
+          waitpid(childPid, &childStatus, WNOHANG | WUNTRACED);
+          char* bgPid;
+          asprintf(&bgPid, "%jd", (intmax_t)childPid);
+          setenv("BG_PID", bgPid, 1);
+        }
+      }  // End of defualt
+    }  // End of switch case 
+  }  // End of infinite loop
+}  //End of main
 
 char *words[MAX_WORDS] = {0};
-
 
 /* Splits a string into words delimited by whitespace. Recognizes
  * comments as '#' at the beginning of a word, and backslash escapes.
@@ -171,13 +407,31 @@ expand(char const *word)
   build_str(NULL, NULL);
   build_str(pos, start);
   while (c) {
-    if (c == '!') build_str("<BGPID>", NULL);
-    else if (c == '$') build_str("<PID>", NULL);
-    else if (c == '?') build_str("<STATUS>", NULL);
+    if (c == '!') {
+      char* bg_pid = getenv("BG_PID");
+      if (!bg_pid) build_str("", NULL);
+      else build_str(bg_pid, NUll);
+    }
+    else if (c == '$') {;
+      char* pid;
+      asprintf(&pid, "%jd", (intmax_t)getpid());
+      if (pid < 0) err(1, "getpid() or asprintf() failure");
+      build_str(pid, NULL);
+    }
+    else if (c == '?') {
+      char* fg_status = getenv("LATEST_FG");
+      if (!fg_status) build_str("0", NULL);
+      else build_str(fg_status, NULL);
+    }
     else if (c == '{') {
-      build_str("<Parameter: ", NULL);
-      build_str(start + 2, end - 1);
-      build_str(">", NULL);
+      size_t length = end - 1 - (start + 2);
+      char temp[length + 1];
+      memcpy(temp, start + 2, length);
+      temp[length] = '\0';  // add null char to end string
+
+      char* value = getenv(temp);
+      if (!value) build_str("", NULL);
+      else build_str(value, NULL);
     }
     pos = end;
     c = param_scan(pos, &start, &end);
